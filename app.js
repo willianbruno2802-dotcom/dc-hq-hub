dafe9f7ea689ed1565451e43a945d3b79203ae0f
willianbruno2802-dotcom <willianbruno2802@gmail.com>
2026-09-20 12:46:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -96,8 +96,6 @@ function renderComics() {
   grid.innerHTML = "";
 grid.innerHTML = "";
 
-grid.innerHTML = "<h2>TESTE — O GRID ESTÁ FUNCIONANDO</h2>";
-
 filtered.forEach(comic => {
   filtered.forEach(comic => {
     const card = document.createElement("article");
